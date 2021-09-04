5c306d2f4c9721163736f9d7ef4c12776037ada0
Jesus Marcano <jsusmarcano.jm@gmail.com>
2021-09-04 00:40:02 -0400

    "Finished Gatsby Tuturial Part 2"

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm a venezuelan programer, i like the chest, the animals, the videogames
+        and a litle the sport ( because i am so bad in sports :( )</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
